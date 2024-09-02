2133f90e9de434d414e83ccd126bb743bcfd57d9
M.Ali <syedalirizvi377@gmail.com>
2024-09-02 16:03:54 +0500

    modified block file

--- block.rb
+++ block.rb
@@ -4,3 +4,15 @@ numbers = [1, 2, 3, 4, 5]
 numbers.each do |number|
   puts number * 2
 end
+
+#adding another block in the block file 
+# passing a block to a custom method
+def greet(name)
+puts "Hello, #{name}!"
+yield if block_given?
+end
+
+# Call the method with a block
+greet("Alice") do
+puts "Nice to meet you!"
+end
